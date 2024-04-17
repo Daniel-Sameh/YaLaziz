8b80064365e6c301160f26cead4e670253dcecb9
Daniel-Sameh <111789605+Daniel-Sameh@users.noreply.github.com>
2024-04-17 18:08:36 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Navigation Bar toggle function:
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
           nav = document.getElementById(navId)
@@ -11,13 +12,14 @@ const showMenu = (toggleId, navId) =>{
     })
  }
  
- showMenu('navToggle','links');
+showMenu('navToggle','links');
+//---------------------------------------------------------
 
-
- var deleteButtons = document.querySelectorAll('.deleteRecipe');
+//Delete Recipe :
+var deleteButtons = document.querySelectorAll('.deleteRecipe');
 
  // Add event listener to each delete button
- deleteButtons.forEach(function(button) {
+deleteButtons.forEach(function(button) {
      button.addEventListener('click', function(event) {
          // Prevent the default action of the button
          event.preventDefault();
@@ -34,18 +36,8 @@ const showMenu = (toggleId, navId) =>{
              }
          }
      });
- });
-
-
-var username= getData('username');
-if(username){
-    document.querySelector('.account').firstChild.innerHTML= username;
-    document.querySelector('.account').firstChild.style='text-decoration:underline;';
-}
-
-
-var isAdmin= getData('admin');
-
+});
+//-----------------------------------------------------------
 function saveData(key, value) {
     localStorage.setItem(key, value);
 }
@@ -53,20 +45,30 @@ function saveData(key, value) {
 function getData(key) {
     return localStorage.getItem(key);
 }
-
 function resetData(){
     try {
         localStorage.removeItem('admin');
         localStorage.removeItem('username');
         localStorage.removeItem('sign');
+        localStorage.removeItem('password');
         console.log("Data reset successful.");
-        location.reload();
+        window.location.reload();
       } catch (error) {
         console.error("Error resetting data:", error);
       }
 }
 
-if(isAdmin=='true'){
+//Account Details:
+var username= getData('username');
+var isSignned= getData('sign');
+if(username&&isSignned){
+    document.querySelector('.account').firstChild.innerHTML= username;
+    document.querySelector('.account').firstChild.style='text-decoration:underline;';
+}
+
+
+var isAdmin= getData('admin');
+if(isAdmin=='true'&&isSignned){
     var cards = document.querySelectorAll('.recipe');
     cards.forEach(function(card) {
              // Get the adminContainer within each card
@@ -80,39 +82,21 @@ if(isAdmin=='true'){
     console.log('admin is null!');
 }
 
-var signedIn= getData('sign');
-if(signedIn){
+
+if(isSignned){
     document.querySelector('.logout').style.display = 'flex';
     document.querySelector('.logout').style.alignItems= 'center';
     document.querySelector('.login').style.display = 'none';
     document.querySelector('.sign_up').style.display = 'none';
-
 }
 
-favoriteButtons = document.querySelectorAll(".favorite_buttons");
-
-favoriteButtons.forEach(button => {
-   button.addEventListener("click", function() {
-       const addTitle = "Add to Your favorites";
-       const removeTitle = "Remove from Your favorites";
-       img = button.querySelector("img");
-       if (img.title == addTitle) {
-           img.src = "../../Photos/heartRed.png";
-           img.title = removeTitle;
-       }
-       else {
-           img.src = "../../Photos/heartWhite.png"
-           img.title = addTitle;
-       }
-       img.style.opacity = 0;
-       setTimeout(() => {
-           img.style.opacity = 1; 
-       }, 100);
-   });
- });
 
 
- favoriteButtonsMain = document.querySelectorAll(".favorite_buttons_main");
+
+//-----------------------------------------------------------------------
+
+//For Recipes:
+favoriteButtonsMain = document.querySelectorAll(".favorite_buttons_main");
 
 favoriteButtonsMain.forEach(button => {
    button.addEventListener("click", function() {
@@ -135,16 +119,38 @@ favoriteButtonsMain.forEach(button => {
  });
 
 
+//--------------------------------------------------------------------
 
-/*  document.querySelector('.logout').addEventListener("click",function(event){
-     var logOut=window.confirm("Are you sure to log out from YaLaziz?");
-     if(logOut){
-         document.querySelector('.logout').style.display = 'none';
-         document.querySelector('.login').style.alignItems= 'center';
-         document.querySelector('.sign_up').style.alignItems= 'center';
-         document.querySelector('.login').style.display = 'flex';
-         document.querySelector('.sign_up').style.display = 'flex';
-         resetData();
-     }
+ document.querySelector('.logout').addEventListener('click',function(event){
+    var logOut=window.confirm("Are you sure to log out from YaLaziz?");
+    if(logOut){
+        document.querySelector('.logout').style.display = 'none';
+        document.querySelector('.login').style.alignItems= 'center';
+        document.querySelector('.sign_up').style.alignItems= 'center';
+        document.querySelector('.login').style.display = 'flex';
+        document.querySelector('.sign_up').style.display = 'flex';
+        resetData();
+    }
     
- }); */
+});
+//----------------------------------------------------------------------
+
+document.querySelector('.account').addEventListener('click', function(event){
+    event.preventDefault();
+    if(!isSignned){
+        // const note= document.createElement('ul');
+        // note.classList.add("account");
+        // note.innerHTML="Your are not Logged In yet!";
+        // note.style.zIndex=2000;
+        // note.style.color="red";
+        // note.style.backgroundColor="#FDA403";
+        // note.style.width="100px";
+        // const acc=document.querySelector('.account');
+        // acc.appendChild(note);
+        alert("You are not logged in yet!");
+        window.location.href="login.html";
+    }else{
+        window.location.href="my account.html";
+    }
+})
+
